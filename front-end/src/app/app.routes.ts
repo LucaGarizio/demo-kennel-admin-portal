50980c40aa9418834ba9d5f0021187a8a378e275
Luca Garizio <garizioluca2@gmail.com>
2026-02-19 21:24:58 +0100

    refactor: implement lazy loading for all route components using `loadComponent`

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,29 +1,19 @@
 import { Routes } from '@angular/router';
-import { Login } from './pages/login/login';
-import { DogList } from './pages/dog/dog-list/dog-list';
-import { OwnerList } from './pages/owner/owner-list/owner-list';
-import { OwnerManageComponent } from './pages/owner/owner-manage/owner-manage';
-import { DogManageComponent } from './pages/dog/dog-manage/dog-manage';
-import { StayList } from './pages/stay/stay-list/stay-list';
-import { StayManageComponent } from './pages/stay/stay-manage/stay-manage';
 import { AuthGuard } from './auth.guard';
-import { KennelScheduleComponent } from './pages/kennel-schedule/kennel-schedule';
-import { SignaturePage } from './pages/signature-page/signature-page';
-import { SignatureWaiting } from './pages/signature-waiting/signature-waiting';
 
 export const routes: Routes = [
-  { path: '', component: Login },
+  { path: '', loadComponent: () => import('./pages/login/login').then(m => m.Login) },
 
-  { path: 'lista-cani', component: DogList, canActivate: [AuthGuard] },
-  { path: 'cane/:id', component: DogManageComponent, canActivate: [AuthGuard] },
+  { path: 'lista-cani', loadComponent: () => import('./pages/dog/dog-list/dog-list').then(m => m.DogList), canActivate: [AuthGuard] },
+  { path: 'cane/:id', loadComponent: () => import('./pages/dog/dog-manage/dog-manage').then(m => m.DogManageComponent), canActivate: [AuthGuard] },
 
-  { path: 'lista-proprietari', component: OwnerList, canActivate: [AuthGuard] },
-  { path: 'proprietario/:id', component: OwnerManageComponent, canActivate: [AuthGuard] },
+  { path: 'lista-proprietari', loadComponent: () => import('./pages/owner/owner-list/owner-list').then(m => m.OwnerList), canActivate: [AuthGuard] },
+  { path: 'proprietario/:id', loadComponent: () => import('./pages/owner/owner-manage/owner-manage').then(m => m.OwnerManageComponent), canActivate: [AuthGuard] },
 
-  { path: 'lista-soggiorni', component: StayList, canActivate: [AuthGuard] },
-  { path: 'soggiorno/:id', component: StayManageComponent, canActivate: [AuthGuard] },
+  { path: 'lista-soggiorni', loadComponent: () => import('./pages/stay/stay-list/stay-list').then(m => m.StayList), canActivate: [AuthGuard] },
+  { path: 'soggiorno/:id', loadComponent: () => import('./pages/stay/stay-manage/stay-manage').then(m => m.StayManageComponent), canActivate: [AuthGuard] },
 
-  { path: 'box', component: KennelScheduleComponent, canActivate: [AuthGuard] },
-  { path: 'kiosk', component: SignatureWaiting },
-  { path: 'kiosk/:sessionId', component: SignaturePage },
+  { path: 'box', loadComponent: () => import('./pages/kennel-schedule/kennel-schedule').then(m => m.KennelScheduleComponent), canActivate: [AuthGuard] },
+  { path: 'kiosk', loadComponent: () => import('./pages/signature-waiting/signature-waiting').then(m => m.SignatureWaiting) },
+  { path: 'kiosk/:sessionId', loadComponent: () => import('./pages/signature-page/signature-page').then(m => m.SignaturePage) },
 ];
